354849de806830379cd3e624172aa88adfb77bdc
T2P\sang.luc <sang.luc@travel2pay.com>
2022-09-16 19:42:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import "./App.css";
-import { Component } from "react";
+import './App.css';
+import { Component } from 'react';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: "121",
-          name: "Frank",
-        },
-        {
-          id: "122",
-          name: "Jacky",
-        },
-        {
-          id: "123",
-          name: "Andrei",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  async componentDidMount() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    this.setState({
+      monsters: data,
+    });
   }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.trim().toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) =>
+              monster.name.toLocaleLowerCase().includes(searchString)
+            );
+
+            this.setState({ monsters: filteredMonsters });
+          }}
+        />
+
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
